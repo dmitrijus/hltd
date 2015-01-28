4b190435181026aa98b33d7ec94889eda3353141
Srecko Morovic <srecko.morovic@cern.ch>
2015-01-28 15:44:37 +0100

    run fuser -km as non-root

--- python/hltd.py
+++ python/hltd.py
@@ -193,11 +193,14 @@ def cleanup_mountpoints(remount=True):
                 subprocess.check_call(['umount',os.path.join('/'+point,conf.ramdisk_subdirectory)])
             except subprocess.CalledProcessError, err1:
                 logger.info("trying to kill users of ramdisk")
-                #try:
-                #
-                #   subprocess.check_call(['fuser','-km',os.path.join('/'+point,conf.ramdisk_subdirectory)])
-                #except subprocess.CalledProcessError, err2:
-                #    logger.error("Error calling umount in cleanup_mountpoints (ramdisk), return code:"+str(err2.returncode))
+                try:
+                    nsslock.acquire()
+                    f_user = subprocess.Popen([['fuser','-km',os.path.join('/'+point,conf.ramdisk_subdirectory)],shell=False,preexec_fn=preexec_function,close_fds=True)
+                    f_user.wait()
+                    nsslock.release()
+                except:
+                    try:nsslock.release()
+                    except:pass
                 try:
                     subprocess.check_call(['umount',os.path.join('/'+point,conf.ramdisk_subdirectory)])
                 except subprocess.CalledProcessError, err2:
@@ -207,10 +210,14 @@ def cleanup_mountpoints(remount=True):
                 subprocess.check_call(['umount',os.path.join('/'+point,conf.output_subdirectory)])
             except subprocess.CalledProcessError, err1:
                 logger.info("trying to kill users of output")
-                #try:
-                #    subprocess.check_call(['fuser','-km',os.path.join('/'+point,conf.output_subdirectory)])
-                #except subprocess.CalledProcessError, err2:
-                #    logger.error("Error calling umount in cleanup_mountpoints (output), return code:"+str(err2.returncode))
+                try:
+                    nsslock.acquire()
+                    f_user = subprocess.Popen([['fuser','-km',os.path.join('/'+point,conf.ramdisk_subdirectory)],shell=False,preexec_fn=preexec_function,close_fds=True)
+                    f_user.wait()
+                    nsslock.release()
+                except:
+                    try:nsslock.release()
+                    except:pass
                 try:
                     subprocess.check_call(['umount',os.path.join('/'+point,conf.output_subdirectory)])
                 except subprocess.CalledProcessError, err2:

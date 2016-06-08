4b20a2472ebc180a68669facda2b5b8afb0091f1
Srecko Morovic <srecko.morovic@cern.ch>
2016-06-08 11:02:29 +0200

    added scratchpy dir

--- scripts/hltdrpm.sh
+++ scripts/hltdrpm.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
-# numArgs=$#
-# if [ $numArgs -lt 4 ]; then
-#     echo "Usage: patch-cmssw-build.sh CMSSW_X_Y_Z patchId {dev|pro|...} patchdir"
-#     exit -1
-# fi
-# CMSSW_VERSION=$1            # the CMSSW version, as known to scram
-# PATCH_ID=$2                 # an arbitrary tag which identifies the extra code (usually, "p1", "p2", ...)
-# AREA=$3                     # "pro", "dev", etc...
-# LOCAL_CODE_PATCHES_TOP=$4   # absolute path to the area where extra code to be compiled in can be found, equivalent to $CMSSW_BASE/src
 alias python=python2.6
 # set the RPM build architecture
 #BUILD_ARCH=$(uname -i)      # "i386" for SLC4, "x86_64" for SLC5
@@ -17,16 +8,12 @@ cd $SCRIPTDIR/..
 BASEDIR=$PWD
 
 # create a build area
-
 echo "removing old build area"
 rm -rf /tmp/hltd-build-tmp-area
 echo "creating new build area"
 mkdir  /tmp/hltd-build-tmp-area
-ls
 cd     /tmp/hltd-build-tmp-area
 TOPDIR=$PWD
-ls
-
 
 echo "Moving files to their destination"
 mkdir -p var/log/hltd
@@ -39,10 +26,6 @@ mkdir -p etc/appliance/resources/online
 mkdir -p etc/appliance/resources/except
 mkdir -p etc/appliance/resources/quarantined
 mkdir -p etc/appliance/resources/cloud
-mkdir -p usr/lib64/python2.6/site-packages
-mkdir -p usr/lib64/python2.6/site-packages/pyelasticsearch
-mkdir -p usr/lib64/python2.6/site-packages/elasticsearch
-mkdir -p usr/lib64/python2.6/site-packages/urllib3_hltd
 ls
 cp -r $BASEDIR/python/hltd $TOPDIR/etc/init.d/hltd
 cp -r $BASEDIR/python/soap2file $TOPDIR/etc/init.d/soap2file
@@ -63,141 +46,7 @@ mkdir -p etc/appliance/dqm_resources/except
 mkdir -p etc/appliance/dqm_resources/quarantined
 mkdir -p etc/appliance/dqm_resources/cloud
 
-
-cd $TOPDIR
-#urllib3 1.10 (renamed urllib3_hltd)
-cd opt/hltd/lib/urllib3-1.10/
-python ./setup.py -q build
-python - <<'EOF'
-import compileall
-compileall.compile_dir("build/lib/urllib3_hltd",quiet=True)
-EOF
-python -O - <<'EOF'
-import compileall
-compileall.compile_dir("build/lib/urllib3_hltd",quiet=True)
-EOF
-cp -R build/lib/urllib3_hltd/* $TOPDIR/usr/lib64/python2.6/site-packages/urllib3_hltd/
-
-cd $TOPDIR
-#pyelasticsearch
-cd opt/hltd/lib/pyelasticsearch-1.0/
-python ./setup.py -q build
-python - <<'EOF'
-import compileall
-compileall.compile_dir("build/lib/pyelasticsearch/",quiet=True)
-EOF
-python -O - <<'EOF'
-import compileall
-compileall.compile_dir("build/lib/pyelasticsearch/",quiet=True)
-EOF
-cp -R build/lib/pyelasticsearch/* $TOPDIR/usr/lib64/python2.6/site-packages/pyelasticsearch/
-cp -R pyelasticsearch.egg-info/ $TOPDIR/usr/lib64/python2.6/site-packages/pyelasticsearch/
-
-
-cd $TOPDIR
-#elasticsearch-py
-cd opt/hltd/lib/elasticsearch-py-1.4/
-python ./setup.py -q build
-python - <<'EOF'
-import compileall
-compileall.compile_dir("build/lib/elasticsearch",quiet=True)
-EOF
-python -O - <<'EOF'
-import compileall
-compileall.compile_dir("build/lib/elasticsearch",quiet=True)
-EOF
-cp -R build/lib/elasticsearch/* $TOPDIR/usr/lib64/python2.6/site-packages/elasticsearch/
-
-
-cd $TOPDIR
-#_zlibextras library
-cd opt/hltd/lib/python-zlib-extras-0.1/
-rm -rf build
-python ./setup.py -q build
-cp -R build/lib.linux-x86_64-2.6/_zlibextras.so $TOPDIR/usr/lib64/python2.6/site-packages/
-
-
-cd $TOPDIR
-#python-prctl
-cd opt/hltd/lib/python-prctl/
-./setup.py -q build
-python - <<'EOF'
-import py_compile
-py_compile.compile("build/lib.linux-x86_64-2.6/prctl.py")
-EOF
-python -O - <<'EOF'
-import py_compile
-py_compile.compile("build/lib.linux-x86_64-2.6/prctl.py")
-EOF
-cp build/lib.linux-x86_64-2.6/prctl.pyo $TOPDIR/usr/lib64/python2.6/site-packages
-cp build/lib.linux-x86_64-2.6/prctl.py $TOPDIR/usr/lib64/python2.6/site-packages
-cp build/lib.linux-x86_64-2.6/prctl.pyc $TOPDIR/usr/lib64/python2.6/site-packages
-cp build/lib.linux-x86_64-2.6/_prctl.so $TOPDIR/usr/lib64/python2.6/site-packages
-cat > $TOPDIR/usr/lib64/python2.6/site-packages/python_prctl-1.5.0-py2.6.egg-info <<EOF
-Metadata-Version: 1.0
-Name: python-prctl
-Version: 1.5.0
-Summary: Python(ic) interface to the linux prctl syscall
-Home-page: http://github.com/seveas/python-prctl
-Author: Dennis Kaarsemaker
-Author-email: dennis@kaarsemaker.net
-License: UNKNOWN
-Description: UNKNOWN
-Platform: UNKNOWN
-Classifier: Development Status :: 5 - Production/Stable
-Classifier: Intended Audience :: Developers
-Classifier: License :: OSI Approved :: GNU General Public License (GPL)
-Classifier: Operating System :: POSIX :: Linux
-Classifier: Programming Language :: C
-Classifier: Programming Language :: Python
-Classifier: Topic :: Security
-EOF
-
-cd $TOPDIR
-cd opt/hltd/lib/python-inotify-0.5/
-./setup.py -q build
-cp build/lib.linux-x86_64-2.6/inotify/_inotify.so $TOPDIR/usr/lib64/python2.6/site-packages
-cp build/lib.linux-x86_64-2.6/inotify/watcher.py $TOPDIR/usr/lib64/python2.6/site-packages
-python - <<'EOF'
-import py_compile
-py_compile.compile("build/lib.linux-x86_64-2.6/inotify/watcher.py")
-EOF
-cp build/lib.linux-x86_64-2.6/inotify/watcher.pyc $TOPDIR/usr/lib64/python2.6/site-packages/
-cat > $TOPDIR/usr/lib64/python2.6/site-packages/python_inotify-0.5.egg-info <<EOF
-Metadata-Version: 1.0
-Name: python-inotify
-Version: 0.5
-Summary: Interface to Linux inotify subsystem
-Home-page: 'http://www.serpentine.com/
-Author: Bryan O'Sullivan
-Author-email: bos@serpentine.com
-License: LGPL
-Platform: Linux
-Classifier: Development Status :: 5 - Production/Stable
-Classifier: Environment :: Console
-Classifier: Intended Audience :: Developers
-Classifier: License :: OSI Approved :: LGPL
-Classifier: Natural Language :: English
-Classifier: Operating System :: POSIX :: Linux
-Classifier: Programming Language :: Python
-Classifier: Programming Language :: Python :: 2.4
-Classifier: Programming Language :: Python :: 2.5
-Classifier: Programming Language :: Python :: 2.6
-Classifier: Programming Language :: Python :: 2.7
-Classifier: Topic :: Software Development :: Libraries :: Python Modules
-Classifier: Topic :: System :: Filesystems
-Classifier: Topic :: System :: Monitoring
-EOF
-
-
-cd $TOPDIR
-cd opt/hltd/lib/python-procname/
-./setup.py -q build
-cp build/lib.linux-x86_64-2.6/procname.so $TOPDIR/usr/lib64/python2.6/site-packages
-
 cd $TOPDIR/opt/hltd/bin
-#gcc turbostat.c -o turbostat
-rm -rf turbostat.c
 
 rm -rf $TOPDIR/opt/hltd/rpm
 rm -rf $TOPDIR/opt/hltd/lib
@@ -225,8 +74,7 @@ Provides:/etc/hltd.conf
 Provides:/etc/logrotate.d/hltd
 Provides:/etc/init.d/hltd
 Provides:/etc/init.d/soap2file
-Provides:/usr/lib64/python2.6/site-packages/prctl.pyc
-Requires:python,libcap,python-six >= 1.4 ,python-requests,SOAPpy,python-simplejson >= 3.3.1,jsonMerger,python-psutil
+Requires:hltd-libs >= 1.9.6,SOAPpy,python-simplejson >= 3.3.1,jsonMerger,python-psutil
 
 %description
 fff hlt daemon
@@ -241,13 +89,11 @@ mkdir -p \$RPM_BUILD_ROOT
 %__install -d "%{buildroot}/var/log/hltd/pid"
 tar -C $TOPDIR -c opt/hltd | tar -xC \$RPM_BUILD_ROOT
 tar -C $TOPDIR -c etc | tar -xC \$RPM_BUILD_ROOT
-tar -C $TOPDIR -c usr | tar -xC \$RPM_BUILD_ROOT
 rm \$RPM_BUILD_ROOT/opt/hltd/python/setupmachine.py
 rm \$RPM_BUILD_ROOT/opt/hltd/python/disablenode.py
 rm \$RPM_BUILD_ROOT/opt/hltd/python/dbcheck.py
 rm \$RPM_BUILD_ROOT/opt/hltd/TODO
 %post
-#/opt/hltd/python/fillresources.py #--> in fffmeta
 %files
 %dir %attr(777, -, -) /var/log/hltd
 %dir %attr(777, -, -) /var/log/hltd/pid
@@ -258,15 +104,6 @@ rm \$RPM_BUILD_ROOT/opt/hltd/TODO
 /etc/init.d/hltd
 /etc/init.d/soap2file
 /etc/appliance
-/usr/lib64/python2.6/site-packages/*prctl*
-/usr/lib64/python2.6/site-packages/*watcher*
-/usr/lib64/python2.6/site-packages/*_inotify.so*
-/usr/lib64/python2.6/site-packages/*python_inotify*
-/usr/lib64/python2.6/site-packages/*_zlibextras.so
-/usr/lib64/python2.6/site-packages/pyelasticsearch
-/usr/lib64/python2.6/site-packages/elasticsearch
-/usr/lib64/python2.6/site-packages/urllib3_hltd
-/usr/lib64/python2.6/site-packages/procname.so
 %preun
 if [ \$1 == 0 ]; then
   /sbin/service hltd stop || true
